29de9cc518cf65bc348e198bcbb7b0ec5f294163
HoangNam <hnam.19498@outlook.com>
2022-12-02 14:42:20 +0700

    fixed

---
+++ om_hospital/models/patient.py
@@ -0,0 +1,45 @@
+from odoo import api, fields, models
+from datetime import date
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of birth')
+    ref = fields.Char(string='Reference')
+    age = fields.Integer(string='Age', tracking=True, compute='_compute_age')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
+    active = fields.Boolean(string='Active', default=True)
+    appointment_id = fields.Many2one(comodel_name='hospital.appointment', string='Appointment')
+    image = fields.Image(string='Image')
+    tag_ids = fields.Many2many('patient.tag', string='Tags')
+
+    @api.model
+    def create(self, vals):
+        vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).create(vals)
+
+    def write(self, vals):
+        if not self.ref and not vals.get('ref'):
+            vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).write(vals)
+
+    @api.depends("date_of_birth")
+    def _compute_age(self):
+        for h in self:
+            today = date.today()
+            if h.date_of_birth:
+                h.age = today.year - h.date_of_birth.year
+            else:
+                h.age = 1
+
+    def name_get(self):
+        patient_list = []
+        for rec in self:
+            name = f'{rec.ref} - {rec.name}'
+            patient_list.append((rec.id, name))
+
+        return patient_list
