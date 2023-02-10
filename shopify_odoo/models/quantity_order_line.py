e0724021c83882ad999a13b90679ba9036b89ad4
hnam19498 <hnam.19498@outlook.com>
2023-02-10 11:44:52 +0700

    fix

--- shopify_odoo/models/quantity_order_line.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, models, fields, _
-
-
-class QuantityOrderLine(models.Model):
-    _name = 'quantity.order.line'
-
-    product_id = fields.Many2one('shopify.product')
-    order_id = fields.Many2one('shopify.order')
-    quantity = fields.Float()
